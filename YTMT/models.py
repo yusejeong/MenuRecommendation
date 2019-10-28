20d3cc3a37251537335b8876a583c8799fba5a44
eunjeong <lm040466@gmail.com>
2019-10-28 15:42:17 +0900

    Collab

--- YTMT/models.py
+++ YTMT/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=10)
+    pw = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+
+    birth = models.DateField()
+    gender = 1
+
+    religion = []
+    vegetarian = []
+
+    allergie = []
+    hate_food = []
+    hate_ingredient = []
+    
+class History(models.Model):
+    user_id
+    history = []
+
+class Friendlist(models.Model):
+    user_id
+    friend_id = []
+
+class Religion(models.Model):
+    reli_id
+    reli_name
+    reli_prohibited = []
+
+class Vegetarian(models.Model):
+    vege_id
+    vege_name
+    vege_prohibited = []
+
+class Menu(models.Model):
+    menu_id
+    menu_name
+    ingredient = []
+    PendingDeprecationWarning
