5b4d9da3495ed21a336635efe8a48cc4f78b7eb0
eunjeong <lm040466@gmail.com>
2019-11-18 23:28:59 +0900

    hot fix

--- YTMT/views.py
+++ YTMT/views.py
@@ -307,12 +307,12 @@ def menureco(request):
     login_user = find_user(request)
     userProfile = Profile.objects.get(user_id = login_user)
 
-    #사용자의 싫어하는 메뉴, 싫어하는 재료, 알레르기 리스트를 로드함
+    # 사용자의 싫어하는 메뉴, 싫어하는 재료, 알레르기 리스트를 로드함
     hate_menu_list = Hate_menu.objects.filter(user_id = login_user)
     hate_ingredient_list = Hate_ingredient.objects.filter(user_id = login_user)
     Allergy_list = Allergy.objects.filter(user_id = login_user)
 
-    #사용자의 좋아했던 메뉴를 로드
+    # 사용자의 좋아했던 메뉴를 로드
     history_list = History.objects.filter(user_id = login_user)
     menu_list = []
 
@@ -324,9 +324,9 @@ def menureco(request):
 
     return render(request, 'menureco/menureco.html',{ 'menu_list': menu_list })
 
-    #친구와 함께 기능!
-    # def friendreco(request):
-    #     login_user=find_user(request)
-    #     userProfile=Profile.objects.get(user_id=login_user)
-    #     friend_list=Friend
-    #     return render(request, 'menureco/friendreco.html')
+    # 친구와 함께 기능!
+    def friendreco(request):
+        login_user=find_user(request)
+        userProfile=Profile.objects.get(user_id=login_user)
+        friend_list=Friend_list.objects.filter(user_id = login_user)
+        return render(request, 'menureco/friendreco.html')
