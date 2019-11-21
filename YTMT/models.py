a96d5897d57a8889c8cfefbc2451d113a668f648
SeungJu Baek <halucinor0@gmail.com>
2019-11-21 17:18:37 +0900

    필터 적용 메뉴 추천 기능 완성

--- YTMT/models.py
+++ YTMT/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import auth
-
 # 메뉴
 class Menu(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, blank = False, null = False)
-    img_url = models.ImageField(null=True, max_length=100)
-    text = models.TextField(null=True)
+    img_url = models.ImageField(null=True, max_length=100, blank=True)
+    text = models.TextField(null=True, blank=True)
 
     likes = models.IntegerField(default= 0)
 
--- YTMT/views/main.py
+++ YTMT/views/main.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from YTMT.models import *
 from django.contrib import auth
 import datetime
+import random
 from . import session as SS
 
 import os
@@ -29,16 +30,88 @@ def menureco(request):
     hate_menu_list = Hate_menu.objects.filter(user_id = login_user)
     hate_ingredient_list = Hate_ingredient.objects.filter(user_id = login_user)
     Allergy_list = Allergy.objects.filter(user_id = login_user)
+    
+    #사용자의 종교, 채식주의 성향 확인
+    user_religion_type = userProfile.reli_id
+    user_vege_type = userProfile.vege_id
+    # print("사용자 종교, 채식 성향")
+    # print(user_religion_type, user_vege_type)
+
+    # 종교, 채식주의 성향에 따른 필터 재료 목록 가져오기    
+    vegetarian_list = Vegetarian_case.objects.filter(vege_id = user_vege_type)
+    religion_list = Religion_case.objects.filter(reli_id= user_religion_type)
+
+
+
+
+    filter_list = []
+
+    for hate_religion_ingre in religion_list:
+        # print(type(hate_religion_ingre.ingre))
+        filter_list.append(hate_religion_ingre.ingre.name)
+    # #채식 성향 재료 필터
+    for hate_vege_ingre in vegetarian_list:
+        filter_list.append(hate_vege_ingre.ingre.name)
+    # #싫어하는 재료 필터
+    for hate_ingredient in hate_ingredient_list:        
+        filter_list.append(hate_ingredient.ingre.name)
+    # #알레르기 재료 필터
+    for allergy in Allergy_list:
+        filter_list.append(allergy.ingre.name)
+    
+
+
+    #중복 요소 제거
+    filter_list = list(dict.fromkeys(filter_list))
+
 
     # 사용자의 좋아했던 메뉴를 로드
     history_list = History.objects.filter(user_id = login_user)
+    print(history_list)
     menu_list = []
 
     for food in history_list:
         menu_name = food.menu
         cos = sim_df[str(menu_name)].sort_values(ascending=False)
-        for name in cos.head(3).index:
-            menu_list.append(Menu.objects.get(name = name))
+    
+        for name in cos[1:4].index:
+            menu_obj = Menu.objects.get(name = name)
+            recipes = Recipe.objects.filter(menu = menu_obj)
+            can_append = True
+
+            for recipe in recipes:
+                if recipe.ingre.name in filter_list:
+                    can_append = False
+                    break
+            
+            if can_append:
+                menu_list.append(menu_obj)
+
+    filter_menu = []
+
+    menu_cnt = len(Menu.objects.all())
+    
+    while True:
+        menu_id = random.randint(1, menu_cnt)
+        menu_obj = Menu.objects.get(id = menu_id)
+        recipes = Recipe.objects.filter(menu = menu_obj)
+        can_append = True
+        
+        for recipe in recipes:
+            if recipe.ingre.name in filter_list:
+                # print(menu_obj.name + " has " + recipe.ingre.name)
+                can_append = False
+                break
+        
+        if can_append:
+            # print("append : " + menu_obj.name)
+            filter_menu.append(menu_obj)
+
+        if len(filter_menu) >= 5:
+            break
+
+    menu_list.extend(filter_menu)   
+
 
     return render(request, 'menureco/menureco.html',{ 'menu_list': menu_list })
 
