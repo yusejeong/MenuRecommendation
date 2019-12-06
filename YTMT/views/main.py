2bb5d7c40a707482bc0339c5e757e6a96cfd868f
SeungJu Baek <halucinor0@gmail.com>
2019-12-06 14:42:31 +0900

    hot fix

--- YTMT/views/main.py
+++ YTMT/views/main.py
@@ -86,7 +86,11 @@ def menureco(request):
     menu_cnt = Menu.objects.count()
 
     while True:
-        menu_id = random.randint(1, menu_cnt)
+        menu_id = 0
+        while True:
+            menu_id = random.randint(1, menu_cnt)
+            if Menu.objects.filter(id = menu_id).exists():                
+                break
         menu_obj = Menu.objects.get(id = menu_id)
         recipes = Recipe.objects.filter(menu = menu_obj)
         can_append = True
@@ -166,7 +170,11 @@ def groupmenureco(request):
     menu_cnt = Menu.objects.count()
 
     while True:
-        menu_id = random.randint(1, menu_cnt)
+        menu_id = 0
+        while True:
+            menu_id = random.randint(1, menu_cnt)
+            if Menu.objects.filter(id = menu_id).exists():                
+                break
         menu_obj = Menu.objects.get(id = menu_id)
         recipes = Recipe.objects.filter(menu = menu_obj)
         can_append = True
